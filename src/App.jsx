68dd06c167ce9037f4b40e4752810d8522fbd900
Camilo Lavado <camilolavado.it@gmail.com>
2026-05-01 23:47:17 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>RecruitAPI - Iniciando React</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
